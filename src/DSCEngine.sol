3b33d90940afce5918c7c3a86e4407cee358f93d
reverse-banana <fjkw@gmail.com>
2024-07-19 17:50:17 +0300

    HelperConfig finished strucure

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 import {DecentralizedStableCoin} from "src/DecentralizedStableCoin.sol";
-import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {AggregatorV3Interface} from "@chainlink/contracts/v0.8/interfaces/AggregatorV3Interface.sol";
 
@@ -21,7 +21,7 @@ import {AggregatorV3Interface} from "@chainlink/contracts/v0.8/interfaces/Aggreg
  * It handles all the logic for mining and reediming DSC, as well as depositing & withdrawing collateral
  * @notice This contract is VERY looosely based on the Maker DAO DSS (DAI) system
  */
-contract DCSEngine is ReentrancyGuard {
+contract DSCEngine is ReentrancyGuard {
     ////////////////////////////
     // Errors              //
     ////////////////////////////
@@ -30,14 +30,21 @@ contract DCSEngine is ReentrancyGuard {
     error DCSEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLenght();
     error DCSEngine__NotAllowedToken();
     error DCSEngine__TransferFailed();
+    error DCSEngine__BreakHealthFactor(uint256 healthFactor);
+    error DCSEngine__revertIfHealthFactorIsBroken(address user);
+    error DCSEngine__MintFailed();
 
     ////////////////////////////
     // State variables        //
     ////////////////////////////
-    
+
     uint256 private constant ADDITIONAL_FEED_PRECISION_1e10 = 1e10;
     uint256 private constant PRECISION_1e18 = 1e18;
-    
+
+    uint256 private constant LIQUIDATION_THRESHOLD = 50; 
+    uint256 private constant LIQUIDATION_PRECISION = 100; 
+    uint256 private constant MIN_HEALTH_FACTOR = 1;
+
     mapping(address token => address priceFeed) private s_priceFeeds;
     // the default value for a bool is false. This means that in your mapping s_tokenToAllowed, all token addresses will map to false unless explicitly set to true.
 
@@ -90,7 +97,7 @@ contract DCSEngine is ReentrancyGuard {
         for (uint256 i = 0; i < tokenAddresses.length; i++) {
             s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
             s_collateralTokens.push(tokenAddresses[i]);
-            // creating key array of the s_priceFeeds mapping that we can loop thorught 
+            // creating key array of the s_priceFeeds mapping that we can loop thorught
             // to get the total deposite amount from all available tokens
         }
 
@@ -133,10 +140,14 @@ contract DCSEngine is ReentrancyGuard {
      * @notice they must have more collateral value that minimum threshold
      */
     function mintDsc(uint256 amountDscToMint) external moreThanZero(amountDscToMint) {
-        
-        revertIfHealthFactorIsBroken(msg.sender);
-
+        _revertIfHealthFactorIsBroken(msg.sender);
         s_DSCMinted[msg.sender] += amountDscToMint;
+        // ask question about order of lines
+        bool minted = i_dsc.mint(msg.sender, amountDscToMint);
+        if (!minted) {
+            revert DCSEngine__MintFailed();
+        }
+
     }
 
     function burnDsc() external {}
@@ -145,38 +156,52 @@ contract DCSEngine is ReentrancyGuard {
 
     function getHealthFactor() external view {}
 
-
     ////////////////////////////////////////
     // Privat & Internal view Functions  //
     ///////////////////////////////////////
 
+    /**
+     * @notice  returns how close to liquidations a user is
+     * If  a user goes below 1, they can get liquidated
+     *
+     * @param user - address of the user which health factro we wanna fetch
+     *
+     * @dev - in order to get the health factoe we so=houdl compare the actual amount
+     * of the fund that was minted and allocated in general for it we implement another nested function
+     * that will return the that values for us to make calculation on
+     *
+     */
+    function _healthFactor(address user) private view returns (uint256) {
+        (uint256 totalDscMinted, uint256 collateralValueInUsd) = _getAccountInformation(user);
 
-    function _getAccountInformation(address user) private view returns(uint256 totalDscMinted, uint256 collateralValueInUsd) {
+        uint256 collateralAdjustedForThreshold = (collateralValueInUsd * LIQUIDATION_THRESHOLD) / LIQUIDATION_PRECISION;
+        // $150 ETH / 100 DCS = 1.5;
+        // 150 * 50 = 7500 / 100 = (75 / 100) < 1;
+        // (meaning the user have to be 200 collaralized)
+
+        return (collateralAdjustedForThreshold * LIQUIDATION_PRECISION) / totalDscMinted;
+        // 75 * 100 = 7500 / 100 = 75
+    }
+
+    function _getAccountInformation(address user)
+        private
+        view
+        returns (uint256 totalDscMinted, uint256 collateralValueInUsd)
+    {
         totalDscMinted = s_DSCMinted[user];
         collateralValueInUsd = getAccountCollateralValue(user);
         // will be a public function so other user could monitor health status and buy off the liquidation for each other
     }
 
-    /**
-     * @notice  returns how close to liquidations a user is
-     * If  a user goes below 1, they can get liquidated
-     
-     * @param user - address of the user which health factro we wanna fetch
-     
-     * @dev - in order to get the health factoe we so=houdl compare the actual amount 
-     * of the fund that was minted and allocated in general for it we implement another nested function
-     * that will return the that values for us to make calculation on
-     * 
-     */
-
-    function _healthFactor(address user) private view returns(uint256) {
-
-        (uint256 totalDscMinted, uint256 collateralValueInUsd) = _getAccountInformation(user);
+    function _revertIfHealthFactorIsBroken(address user) internal view {
+        uint256 userHealthFactor = _healthFactor(user);
+        if (userHealthFactor < MIN_HEALTH_FACTOR) {
+            revert DCSEngine__BreakHealthFactor(userHealthFactor);
+        }
     }
 
-    function revertIfHealthFactorIsBroken(address) internal view {
 
-    }
+
 
 
     //////////////////////////////////////////////
@@ -185,9 +210,9 @@ contract DCSEngine is ReentrancyGuard {
 
     /**
      * @dev in order to get the full amount of the collateral of the given account
-     * we have loop throught each collateral token, get the amount the have deposited, 
+     * we have loop throught each collateral token, get the amount the have deposited in general 
      */
-    function getAccountCollateralValue(address user) public view returns(uint256 totalCollateralValueInUsd) {
+    function getAccountCollateralValue(address user) public view returns (uint256 totalCollateralValueInUsd) {
         for (uint256 i = 0; i < s_collateralTokens.length; i++) {
             address token = s_collateralTokens[i];
             // getting the token address at iterator index from the array we passed to constructor
@@ -201,8 +226,7 @@ contract DCSEngine is ReentrancyGuard {
         return totalCollateralValueInUsd;
     }
 
-
-    function getUsdValue(address token, uint256 amount) public view returns(uint256) {
+    function getUsdValue(address token, uint256 amount) public view returns (uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
         (, int256 price,,,) = priceFeed.latestRoundData();
         uint8 tokenDecimalsAmount = priceFeed.decimals();
@@ -210,5 +234,4 @@ contract DCSEngine is ReentrancyGuard {
         // since the eth and btc return a 1e8 decimal value we created a additional 1e10 value to bring the price  into 1e18 decimals format with we would multiply with actual amount and than substract the same 1e18 to get clean number
         // upd: to make the fucntion more flexible I fetch the decimals amount of the token pasted from the priceFeed and then divide it to get clean number (not tested yet)
     }
-
 }
--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -20,7 +20,7 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable {
     error DecentralizedStableCoin__BurnAmountExceedsBalance();
     error DecentralizedStableCoin__NotZeroAddress();
 
-    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {}
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable() {}
 
     function burn(uint256 _amount) public virtual override {
         uint256 balance = balanceOf(msg.sender);
